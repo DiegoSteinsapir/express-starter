2b4d48ac1e8f78ba74b8303a49cde19127fdf19a
DiegoSteinsapir <dasteinsapir@uc.cl>
2016-04-03 17:09:07 -0300

    post and get methods ready

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict'
 const express = require('express')
 const morgan = require('morgan')
 
@@ -8,5 +9,17 @@ app.get('/', (req, res) => {
   // Send JSON
   res.send({ status: 'on' })
 })
+
+var pencils = []
+
+app.post('/pencils', function pencilPost(req, res) {
+  pencils[pencils.length] = req.query
+  res.send('ok')
+})
+
+app.get('/pencils', function pencilGet(req, res) {
+  res.send({pencils})
+})
+
 
 module.exports = app
